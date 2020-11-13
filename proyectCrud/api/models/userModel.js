d56de6d5309856c2c5956b853e48b2494080e462
david1999erazo <david.erazo2@correo.icesi.edu.co>
2020-11-12 19:36:04 -0500

    Add create project and structure

---
+++ proyectCrud/api/models/userModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+    {
+        name: {type: String, required: true},
+        firstname: {type: String, required: true},
+        lastname: {type: String, required: true},
+        username: {type: String, required: true, min: 8},
+        identification: {type: Number, required: true},
+        password: {type: String, required:true, min: 8 },
+        photo: {type: String, required:true},
+        active: {type: Boolean, required:true}
+
+    }
+)
