2e59d095a1b2aee827e4d6523e7035ee1d1b3048
NicolasB2 <nicolas.biojo@correo.icesi.edu.co>
2020-11-15 17:43:00 -0500

    fixed route problem

--- vueprojectcrud/projectCrud/api/routes/userRoutes.js
+++ vueprojectcrud/projectCrud/api/routes/userRoutes.js
@@ -1,11 +1,12 @@
 var express = require('express');
-var router = express.Router();
+var router =  express.Router();
 var user_controller = require("../controllers/userController")  
 
 router.get("/", user_controller.listAllUsers) 
 router.post("/", user_controller.createUser) 
-router.get('/', user_controller.readUser);
-router.put("/", user_controller.updateUser) 
-router.delete("/", user_controller.deleteUser)  
+
+router.get('/:userId', user_controller.readUser);
+router.put("/:userId", user_controller.updateUser) 
+router.delete("/:userId", user_controller.deleteUser)  
 
 module.exports = router; 
